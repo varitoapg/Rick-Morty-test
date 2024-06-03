d2e16727d1e4af572c81528a789e4ed581a0ad6c
Alvaro Parada <alvaro.parada92@gmail.com>
2024-06-03 16:39:44 +0200

    Improve naming of labels

--- src/components/common/Switch/Switch.tsx
+++ src/components/common/Switch/Switch.tsx
@@ -1,10 +1,10 @@
 type SwitchProps = {
   isChecked: boolean;
   onChange: () => void;
-  values: string[];
+  labels: string[];
 };
 
-export const Switch = ({ isChecked, onChange, values }: SwitchProps) => {
+export const Switch = ({ isChecked, onChange, labels }: SwitchProps) => {
   return (
     <div className="flex justify-center mb-4">
       <label className="inline-flex items-center cursor-pointer">
@@ -16,11 +16,11 @@ export const Switch = ({ isChecked, onChange, values }: SwitchProps) => {
           onChange={onChange}
         />
         <span className="mr-3 text-sm font-medium text-white-900 dark:text-gray-300">
-          {values[0]}
+          {labels[0]}
         </span>
         <span className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></span>
         <span className="ms-3 text-sm font-medium text-white-900 dark:text-gray-300">
-          {values[1]}
+          {labels[1]}
         </span>
       </label>
     </div>
